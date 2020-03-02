ed740c38f94ca2268f4aba41a668c9856f061f38
김지나 <jinagold.k@gmail.com>
2020-03-02 21:35:40 +0900

    Modify) 파일 위치

---
+++ RPG/RPG/GameScene/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  RPG
+//
+//  Created by 김지나 on 2020/03/02.
+//  Copyright © 2020 김지나. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    
+    override func didMove(to view: SKView) {
+        
+       
+    }
+}
