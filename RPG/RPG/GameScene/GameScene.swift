cfd910240da4c6efa3eb7bd5a170e0c60a1e3dd2
김지나 <jinagold.k@gmail.com>
2020-07-07 21:29:31 +0900

    공격,스킬,아이템 버튼 추가

--- RPG/RPG/GameScene/GameScene.swift
+++ RPG/RPG/GameScene/GameScene.swift
@@ -14,6 +14,12 @@ class GameScene: SKScene {
     let Player = SKSpriteNode(imageNamed: "RedSwan")
     let controlBase = SKSpriteNode(imageNamed: "ControlBase")
     let controlBall = SKSpriteNode(imageNamed: "ControlBall")
+    let attackButtonBase = SKSpriteNode(imageNamed: "ButtonBase")
+    let attackButton = SKSpriteNode(imageNamed: "AttackButton")
+    let itemButtonBase = SKSpriteNode(imageNamed: "ButtonBase")
+    let itemButton = SKSpriteNode(imageNamed: "ItemButton")
+    let skillButtonBase = SKSpriteNode(imageNamed: "ButtonBase")
+    let skillButton = SKSpriteNode(imageNamed: "SkillButton")
     
     //var touchPoint = CGPoint()
     //viewDidLoad
@@ -27,6 +33,33 @@ class GameScene: SKScene {
         controlBall.zPosition = controlBase.zPosition
         controlBall.alpha = controlBase.alpha
         self.addChild(controlBall)
+        
+        attackButtonBase.position = CGPoint(x: 375, y: -125)
+        attackButtonBase.zPosition = 99
+        attackButtonBase.alpha = 0.5
+        self.addChild(attackButtonBase)
+        attackButton.position = attackButtonBase.position
+        attackButton.zPosition = 100
+        attackButton.alpha = attackButtonBase.alpha
+        self.addChild(attackButton)
+        
+        itemButtonBase.position = CGPoint(x: 350, y: -300)
+        itemButtonBase.zPosition = 99
+        itemButtonBase.alpha = 0.5
+        self.addChild(itemButtonBase)
+        itemButton.position = itemButtonBase.position
+        itemButton.zPosition = 100
+        itemButton.alpha = itemButtonBase.alpha
+        self.addChild(itemButton)
+        
+        skillButtonBase.position = CGPoint(x: 550, y: -200)
+        skillButtonBase.zPosition = 99
+        skillButtonBase.alpha = 0.5
+        self.addChild(skillButtonBase)
+        skillButton.position = skillButtonBase.position
+        skillButton.zPosition = 100
+        skillButton.alpha = skillButtonBase.alpha
+        self.addChild(skillButton)
         
         Player.zPosition = 10
         self.addChild(Player)
